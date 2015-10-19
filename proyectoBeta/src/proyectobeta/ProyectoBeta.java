60cd47fe4e7e88cd5729bbfaca84e5f0c09e015a
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-19 09:02:44 +0200

    inicio proyecto beta

---
+++ proyectoBeta/src/proyectobeta/ProyectoBeta.java
@@ -0,0 +1,12 @@
+package proyectobeta;
+
+public class ProyectoBeta {
+
+    public static void main(String[] args) {
+       //proyecto Beta
+       //commit inicial
+        
+        
+    }
+    
+}
