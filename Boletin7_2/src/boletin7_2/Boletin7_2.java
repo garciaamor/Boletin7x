080e2df3a33adb0ac235f89cfa93856108feeb54
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-11-03 10:06:55 +0100

    Boletin

---
+++ Boletin7_2/src/boletin7_2/Boletin7_2.java
@@ -0,0 +1,11 @@
+package boletin7_2;
+
+public class Boletin7_2 {
+
+    public static void main(String[] args) {
+    short numero1=Clase7_2.pedirNumShort();
+    short numero2=Clase7_2.pedirNumShort();
+    Clase7_2.comparar(numero1, numero2);
+    }
+    
+}
