88c9785152600c19116bf591147249b2d59c8a35
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-11-04 10:49:19 +0100

    Boletin

---
+++ Boletin 7_5/src/boletin/pkg7_5/Boletin7_5.java
@@ -0,0 +1,14 @@
+
+
+package boletin.pkg7_5;
+
+public class Boletin7_5 {
+
+    public static void main(String[] args) {
+        float num1=Clase7_5.pedirNumFloat();
+        float num2=Clase7_5.pedirNumFloat();
+        float num3=Clase7_5.pedirNumFloat();
+        Clase7_5.CompararNums(num1, num2, num3);
+    }
+    
+}
